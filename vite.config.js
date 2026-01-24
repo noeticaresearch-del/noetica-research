5af35c42b42ab02145f1e4e1f79ee8a24e6d2ac6
Meg <noetica.research@gmail.com>
2026-01-24 09:26:48 +0900

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/metronome-app/", // ← ここをリポジトリ名に合わせる
+  base: "/noetica-metronome/",// ← ここをリポジトリ名に合わせる
 });
